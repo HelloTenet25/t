bc695f38078ee16c9b0abbdb86277d807a6a2137
ash <aszishrai@yahoo.com>
2022-09-04 16:22:57 +0100

    fib(func):optimised to dp

--- fib.py
+++ fib.py
@@ -2,10 +2,14 @@
 
 
 def func(n):
-    if n <= 1:
-        return n 
-    return func(n-1)+func(n-2)
+    dp = [-1] * (n+1)
+    dp[0] = 0 
+    dp[1] = 1 
 
-print(func(5))
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+print(func(10))
 
 
